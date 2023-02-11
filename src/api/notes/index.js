e5dd8cf8d122bb15fce8eefc6e26cb3d63c5ef71
Afif <afifilhamcaniago@gmail.com>
2023-02-11 15:44:02 +0700

    joi & custom err

--- src/api/notes/index.js
+++ src/api/notes/index.js
@@ -4,8 +4,8 @@ const routes = require('./routes');
 module.exports = {
   name: 'notes',
   version: '1.0.0',
-  register: async (server, { service }) => {
-    const notesHandler = new NotesService(service);
+  register: async (server, { service, validator }) => {
+    const notesHandler = new NotesService(service, validator);
     server.route(routes(notesHandler));
   },
 };
