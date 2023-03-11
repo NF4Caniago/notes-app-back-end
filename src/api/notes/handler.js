4da09562c79135c7c0bb2ac6fe116cc146987210
Afif <afifilhamcaniago@gmail.com>
2023-03-11 15:43:17 +0700

    add normalize db & collaborator

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -64,7 +64,7 @@ class NotesHandler {
     try {
       const { id } = request.params;
       const { id: credentialId } = request.auth.credentials;
-      await this._service.verifyNoteOwner(id, credentialId);
+      await this._service.verifyNoteAccess(id, credentialId);
       const note = await this._service.getNoteById(id);
       return {
         status: 'success',
@@ -78,7 +78,7 @@ class NotesHandler {
           status: 'fail',
           message: error.message,
         });
-        response.code(404);
+        response.code(error.statusCode);
         return response;
       }
       // Server Error
@@ -98,7 +98,7 @@ class NotesHandler {
       const { title, body, tags } = request.payload;
       const { id } = request.params;
       const { id: credentialId } = request.auth.credentials;
-      await this._service.verifyNoteOwner(id, credentialId);
+      await this._service.verifyNoteAccess(id, credentialId);
       await this._service.editNoteById(id, { title, body, tags });
       return {
         status: 'success',
@@ -110,7 +110,7 @@ class NotesHandler {
           status: 'fail',
           message: error.message,
         });
-        response.code(400);
+        response.code(error.statusCode);
         return response;
       }
       // Server Error
@@ -140,7 +140,7 @@ class NotesHandler {
           status: 'fail',
           message: error.message,
         });
-        response.code(404);
+        response.code(error.statusCode);
         return response;
       }
       // Server Error
